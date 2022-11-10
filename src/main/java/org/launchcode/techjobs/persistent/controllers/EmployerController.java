fa07c237a18cd7881e37b4c7d42d71cb3dd46682
gabriellepouncil <gabbiep1993@gmail.com>
2022-11-10 11:50:04 -0600

    Added to Employer and Skill Controller and Repository. Also uncommented TestTaskTwo tests.

--- src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
@@ -1,6 +1,8 @@
 package org.launchcode.techjobs.persistent.controllers;
 
 import org.launchcode.techjobs.persistent.models.Employer;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -13,8 +15,16 @@ import java.util.Optional;
 @RequestMapping("employers")
 public class EmployerController {
 
+    @Autowired // auto-wires bean onto field
+    private EmployerRepository employerRepository;
 
-
+    //Index method that responds at "/employers", with a list of all employers in database
+    //findAll, save, findById (methods we will use)
+    @GetMapping()
+    public String displayEmployerIndex(Model model){
+        model.addAttribute("employers", employerRepository.findAll()); //lists all Employers in index
+        return "employers/index";
+    }
 
     @GetMapping("add")
     public String displayAddEmployerForm(Model model) {
@@ -27,18 +37,20 @@ public class EmployerController {
                                     Errors errors, Model model) {
 
         if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Employer");
             return "employers/add";
         }
 
+        employerRepository.save(newEmployer); //saves new Employer object
         return "redirect:";
     }
 
     @GetMapping("view/{employerId}")
     public String displayViewEmployer(Model model, @PathVariable int employerId) {
 
-        Optional optEmployer = null;
+        Optional<Employer> optEmployer = employerRepository.findById(employerId);
         if (optEmployer.isPresent()) {
-            Employer employer = (Employer) optEmployer.get();
+            Employer employer = optEmployer.get();
             model.addAttribute("employer", employer);
             return "employers/view";
         } else {
